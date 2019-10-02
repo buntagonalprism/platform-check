04b424d7760d1c346e2ef08d7a66197ab8f1e3a2
Alex <alex.bunting314@gmail.com>
2019-10-02 11:30:14 +1000

    Rename method

--- lib/platform_check.dart
+++ lib/platform_check.dart
@@ -13,7 +13,7 @@ class PlatformCheck {
   static const MethodChannel _channel =
       const MethodChannel('com.buntagon/platform_check');
 
-  static Future<Platform> get platformVersion async {
+  static Future<Platform> get platform async {
     final String platformName = await _channel.invokeMethod('getPlatform');
     return Platform.values.firstWhere((Platform platform) => describeEnum(platform) == platformName);
   }
